9eab5db3113c1c4ea0308df95e44a105d7250fee
Jo-Jaehyeon <jerryjo0906@gmail.com>
2025-05-02 22:00:13 +0900

    [Add] AttributeSet

---
+++ Source/A1Game/AbilitySystem/Attributes/A1CharacterAttributeSet.h
@@ -0,0 +1,62 @@
+// Copyright (c) 2025 THIS-ACCENT. All Rights Reserved.
+
+#pragma once
+
+#include "A1AttributeSet.h"
+#include "A1CharacterAttributeSet.generated.h"
+
+UCLASS()
+class A1GAME_API UA1CharacterAttributeSet : public UA1AttributeSet
+{
+	GENERATED_BODY()
+
+public:
+	UA1CharacterAttributeSet();
+
+protected:
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+	virtual void PostAttributeChange(const FGameplayAttribute& Attribute, float OldValue, float NewValue) override;
+
+	//virtual bool PreGameplayEffectExecute(struct FGameplayEffectModCallbackData& Data) override;
+	virtual void PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData& Data) override;
+
+public:
+	ATTRIBUTE_ACCESSORS(ThisClass, Health);
+	ATTRIBUTE_ACCESSORS(ThisClass, MaxHealth);
+	ATTRIBUTE_ACCESSORS(ThisClass, Oxygen);
+	ATTRIBUTE_ACCESSORS(ThisClass, MaxOxygen);
+	ATTRIBUTE_ACCESSORS(ThisClass, Fuel);
+	ATTRIBUTE_ACCESSORS(ThisClass, MaxFuel);
+	ATTRIBUTE_ACCESSORS(ThisClass, Damage);
+
+private:
+	UPROPERTY(BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	FGameplayAttributeData Health;
+
+	UPROPERTY(BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	FGameplayAttributeData MaxHealth;
+
+	UPROPERTY(BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	FGameplayAttributeData Oxygen;
+
+	UPROPERTY(BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	FGameplayAttributeData MaxOxygen;
+
+	UPROPERTY(BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	FGameplayAttributeData Fuel;
+
+	UPROPERTY(BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	FGameplayAttributeData MaxFuel;
+
+	UPROPERTY(BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	FGameplayAttributeData Damage;
+
+
+public:
+	mutable FLyraAttributeEvent OnHealthChanged;
+	mutable FLyraAttributeEvent OnMaxHealthChanged;
+	mutable FAttributeEvent OnOutOfHealth;
+
+private:
+	bool bOutOfHealth;
+};
--- Source/A1Game/Character/Raider/A1RaiderBase.cpp
+++ Source/A1Game/Character/Raider/A1RaiderBase.cpp
@@ -4,6 +4,10 @@
 
 #include "A1LogChannels.h"
 #include "Controller/Raider/A1RaiderController.h"
+#include "AbilitySystem/Attributes/A1CharacterAttributeSet.h"
+#include "GameplayEffect.h"
+#include "Actors/A1EquipmentBase.h"
+#include "Actors/A1PickupableItemBase.h"
 
 #include UE_INLINE_GENERATED_CPP_BY_NAME(A1RaiderBase)
 
@@ -15,6 +19,12 @@ AA1RaiderBase::AA1RaiderBase()
 
 	AIControllerClass = AA1RaiderController::StaticClass();
 	AutoPossessAI = EAutoPossessAI::PlacedInWorldOrSpawned;
+
+	ASC = CreateDefaultSubobject<UAbilitySystemComponent>(TEXT("ASC"));
+	HealthSet = CreateDefaultSubobject<UA1CharacterAttributeSet>(TEXT("AttributeSet"));
+
+	// Register to listen for attribute changes.
+	HealthSet->OnOutOfHealth.AddUObject(this, &AA1RaiderBase::HandleOutOfHealth);
 }
 
 // Called when the game starts or when spawned
@@ -33,10 +43,85 @@ void AA1RaiderBase::Tick(float DeltaTime)
 
 }
 
-// Called to bind functionality to input
-void AA1RaiderBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+void AA1RaiderBase::HandleOutOfHealth(float OldValue, float NewValue)
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	
+	// 애니메이션이 처리될 시간 기다리기
+	GetWorld()->GetTimerManager().ClearTimer(TimerHandle);
+	GetWorld()->GetTimerManager().SetTimer(TimerHandle, this, &AA1RaiderBase::DestroyDueToDeath, 1.5f, false);
+
+
+	GetController()->UnPossess();  // AI가 더 이상 캐릭터를 제어하지 않도록 함
+	
+	
+	if (DeadMontage)
+	{
+		StopAnimMontage();
+		PlayAnimMontage(DeadMontage, 1.0f);
+	}
+}
+
+void AA1RaiderBase::DestroyDueToDeath()
+{
+	Destroy();
+
+	// 천천히 사라지는 효과(연구중)
+	//int32 NumMaterials = GetMesh()->GetNumMaterials();
+	//TArray<UMaterialInstanceDynamic*> DynamicMats;
+	//
+	//for (int32 i = 0; i < NumMaterials; ++i)
+	//{
+	//	UMaterialInstanceDynamic* DynMat = GetMesh()->CreateAndSetMaterialInstanceDynamic(i);
+	//	if (DynMat)
+	//		DynamicMats.Add(DynMat);
+	//}
+	//
+	//if (DynamicMats.Num() == 0) return;
+	//
+	//float FadeDuration = 1.5f;
+	//float TickInterval = 0.05f;
+	//int32 TotalSteps = FadeDuration / TickInterval;
+	//float FadeStep = 1.0f / TotalSteps;
+	//
+	//float* CurrentFade = new float(1.0f);
+	//
+	//FTimerDelegate FadeDelegate;
+	//
+	//FadeDelegate = FTimerDelegate::CreateLambda([=, this]() mutable
+	//	{
+	//		*CurrentFade -= FadeStep;
+	//		float FadeValue = FMath::Clamp(*CurrentFade, 0.0f, 1.0f);
+	//
+	//		for (UMaterialInstanceDynamic* Mat : DynamicMats)
+	//		{
+	//			if (Mat)
+	//				Mat->SetScalarParameterValue(FName("Opacity"), FadeValue);
+	//		}
+	//
+	//		if (FadeValue <= 0.0f)
+	//		{
+	//			GetWorld()->GetTimerManager().ClearTimer(TimerHandle);
+	//			delete CurrentFade;				// 할당 해제
+	//			this->Destroy();
+	//		}
+	//	});
+	//
+	//GetWorld()->GetTimerManager().SetTimer(TimerHandle, FadeDelegate, TickInterval, true);
+
+
+	// 아이템 스폰
+	int ItemNum = dropItems.Num();
+
+	FVector ItemSpawnLocation = GetActorLocation();
+	ItemSpawnLocation.Z = 0;
+
+	//확률에 따라 아이템 설정(일단 무조건 스폰)
+	//확률 정해진게 없어 하드코딩
+	int32 index = FMath::RandRange(0, 100);
+	if (index >= 50)
+		GetWorld()->SpawnActor<AA1EquipmentBase>(dropItems[ItemNum-1], ItemSpawnLocation, GetActorRotation());
+	else
+		GetWorld()->SpawnActor<AA1EquipmentBase>(dropItems[0], ItemSpawnLocation, GetActorRotation());
 
 }
 
