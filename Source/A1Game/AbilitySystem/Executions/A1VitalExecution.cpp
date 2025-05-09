2c19227b641ac404b16ed29d27f684b0e5056fbf
Jo-Jaehyeon <jerryjo0906@gmail.com>
2025-05-09 09:30:18 +0900

    [Add] 기본 체젠

--- Source/A1Game/AbilitySystem/Executions/A1VitalExecution.cpp
+++ Source/A1Game/AbilitySystem/Executions/A1VitalExecution.cpp
@@ -1,54 +1,73 @@
-#include "A1VitalExecution.h"
+// Copyright (c) 2025 THIS-ACCENT. All Rights Reserved.
 
-#include "AbilitySystem/Attributes/A1VitalSet.h"
-#include "AbilitySystem/Attributes/A1CombatSet.h"
+
+#include "AbilitySystem/Executions/A1VitalExecution.h"
+#include "AbilitySystem/Attributes/A1CharacterAttributeSet.h"
+#include "GameplayEffectTypes.h"
 
 #include UE_INLINE_GENERATED_CPP_BY_NAME(A1VitalExecution)
 
 struct FVitalStatics
 {
-	FGameplayEffectAttributeCaptureDefinition BaseHealthDef;
-	FGameplayEffectAttributeCaptureDefinition BaseOxygenDef;
+    FGameplayEffectAttributeCaptureDefinition HealthDef;
+    FGameplayEffectAttributeCaptureDefinition HungerDef;
 
-	FVitalStatics()
-	{
-		BaseHealthDef = FGameplayEffectAttributeCaptureDefinition(UA1CombatSet::GetBaseHealthAttribute(), EGameplayEffectAttributeCaptureSource::Source, true);
-		BaseOxygenDef = FGameplayEffectAttributeCaptureDefinition(UA1CombatSet::GetBaseOxygenAttribute(), EGameplayEffectAttributeCaptureSource::Source, true);
-	}
+    FVitalStatics()
+    {  
+        HealthDef = FGameplayEffectAttributeCaptureDefinition(UA1CharacterAttributeSet::GetHealthAttribute(), EGameplayEffectAttributeCaptureSource::Target, false);
+        HungerDef = FGameplayEffectAttributeCaptureDefinition(UA1CharacterAttributeSet::GetHungerAttribute(), EGameplayEffectAttributeCaptureSource::Target, false);
+    }
 };
 
-static FVitalStatics& HealStatics()
+static FVitalStatics& HungerStatics()
 {
-	static FVitalStatics Statics;
-	return Statics;
+    static FVitalStatics Statics;
+    return Statics;
 }
 
 UA1VitalExecution::UA1VitalExecution()
 {
-	RelevantAttributesToCapture.Add(HealStatics().BaseHealthDef);
-	RelevantAttributesToCapture.Add(HealStatics().BaseOxygenDef);
+    RelevantAttributesToCapture.Add(HungerStatics().HealthDef);
+    RelevantAttributesToCapture.Add(HungerStatics().HungerDef);
 }
 
 void UA1VitalExecution::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
 {
 #if WITH_SERVER_CODE
-	const FGameplayEffectSpec& Spec = ExecutionParams.GetOwningSpec();
+    const FGameplayEffectSpec& Spec = ExecutionParams.GetOwningSpec();
 
-	const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
-	const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+    const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+    const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
 
-	FAggregatorEvaluateParameters EvaluateParameters;
-	EvaluateParameters.SourceTags = SourceTags;
-	EvaluateParameters.TargetTags = TargetTags;
+    FAggregatorEvaluateParameters EvaluateParameters;
+    EvaluateParameters.SourceTags = SourceTags;
+    EvaluateParameters.TargetTags = TargetTags;
 
-	float BaseHealth = 0.0f;
-	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(HealStatics().BaseHealthDef, EvaluateParameters, BaseHealth);
+    float CurrentHealth = 0.0f;
+    ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(HungerStatics().HealthDef, EvaluateParameters, CurrentHealth);
 
-	float BaseOxygen = 0.0f;
-	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(HealStatics().BaseOxygenDef, EvaluateParameters, BaseOxygen);
+    float CurrentHunger = 0.0f;
+    ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(HungerStatics().HungerDef, EvaluateParameters, CurrentHunger);
 
-	OutExecutionOutput.AddOutputModifier(FGameplayModifierEvaluatedData(UA1VitalSet::GetIncomingHealthAttribute(), EGameplayModOp::Additive, BaseHealth));
-	OutExecutionOutput.AddOutputModifier(FGameplayModifierEvaluatedData(UA1VitalSet::GetIncomingOxygenAttribute(), EGameplayModOp::Additive, BaseOxygen));
-	
+    // 5초마다 Health 증가, Hunger 감소
+    OutExecutionOutput.AddOutputModifier(
+        FGameplayModifierEvaluatedData(UA1CharacterAttributeSet::GetHealthAttribute(), EGameplayModOp::Additive, 1.f));
+    OutExecutionOutput.AddOutputModifier(
+        FGameplayModifierEvaluatedData(UA1CharacterAttributeSet::GetHungerAttribute(), EGameplayModOp::Additive, -1.f));
+
+    // Hunger가 120 이하면 weight 증가
+    if (CurrentHunger <= 120.0f)
+    {
+        OutExecutionOutput.AddOutputModifier(
+            FGameplayModifierEvaluatedData(UA1CharacterAttributeSet::GetWeightAttribute(), EGameplayModOp::Additive, 10.f));
+    }
+
+    // Hunger가 0 이하이면 health 감소
+    if (CurrentHunger <= 0.0f)
+    {
+        OutExecutionOutput.AddOutputModifier(
+            FGameplayModifierEvaluatedData(UA1CharacterAttributeSet::GetHealthAttribute(), EGameplayModOp::Additive, -10.f));
+    }
+
 #endif // #if WITH_SERVER_CODE
 }
--- Source/A1Game/AbilitySystem/Executions/A1VitalExecution.h
+++ Source/A1Game/AbilitySystem/Executions/A1VitalExecution.h
@@ -1,16 +1,17 @@
+// Copyright (c) 2025 THIS-ACCENT. All Rights Reserved.
+
 #pragma once
 
 #include "GameplayEffectExecutionCalculation.h"
 #include "A1VitalExecution.generated.h"
 
 UCLASS()
-class UA1VitalExecution : public UGameplayEffectExecutionCalculation
+class A1GAME_API UA1VitalExecution : public UGameplayEffectExecutionCalculation
 {
 	GENERATED_BODY()
 
 public:
-	UA1VitalExecution();
+    UA1VitalExecution();
 
-protected:
-	virtual void Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const override;
+    virtual void Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const override;
 };
