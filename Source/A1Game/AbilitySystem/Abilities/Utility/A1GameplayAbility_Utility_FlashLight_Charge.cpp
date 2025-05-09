cb350aa95fd2b03733e1e39b9aebe62e892d8bff
Jo-Jaehyeon <jerryjo0906@gmail.com>
2025-05-09 21:01:19 +0900

    [Add] 손전등 충전 기능

---
+++ Source/A1Game/AbilitySystem/Abilities/Utility/A1GameplayAbility_Utility_FlashLight_Charge.cpp
@@ -0,0 +1,43 @@
+﻿#include "A1GameplayAbility_Utility_FlashLight_Charge.h"
+
+#include "A1GameplayTags.h"
+#include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
+#include "Actors/A1UtilityBase.h"
+#include "Item/Managers/A1EquipmentManagerComponent.h"
+#include "Item/Managers/A1InventoryManagerComponent.h"
+#include "Item/Managers/A1ItemManagerComponent.h"
+#include "Player/LyraPlayerController.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(A1GameplayAbility_Utility_FlashLight_Charge)
+
+UA1GameplayAbility_Utility_FlashLight_Charge::UA1GameplayAbility_Utility_FlashLight_Charge(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	ActivationRequiredTags.AddTag(A1GameplayTags::Status_MainHand_Left);
+	//ActivationBlockedTags.AddTag(A1GameplayTags::Status_MainHand_Right);
+}
+
+void UA1GameplayAbility_Utility_FlashLight_Charge::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+	
+	if (UAbilityTask_PlayMontageAndWait* PlayMontageTask = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(this, TEXT("ChargeFlashLight"), ChargeMontage, GetSnapshottedAttackRate(), NAME_None, false, 1.f, 0.f, false))
+	{
+		PlayMontageTask->OnCompleted.AddDynamic(this, &ThisClass::OnMontageFinished);
+		PlayMontageTask->ReadyForActivation();
+	}
+
+	AA1UtilityBase* flashlight = Cast<AA1UtilityBase>(GetFirstEquipmentActor());
+	flashlight->ChargeGauge();
+}
+
+void UA1GameplayAbility_Utility_FlashLight_Charge::OnMontageFinished()
+{
+	if (HasAuthority(&CurrentActivationInfo))
+	{
+		//AA1UtilityBase* flashlight = Cast<AA1UtilityBase>(GetFirstEquipmentActor());
+		//flashlight->ChargeGauge();
+
+		EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
+	}
+}
---
+++ Source/A1Game/AbilitySystem/Abilities/Utility/A1GameplayAbility_Utility_FlashLight_Charge.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "AbilitySystem/Abilities/Docker/A1GameplayAbility_Equipment.h"
+#include "A1GameplayAbility_Utility_FlashLight_Charge.generated.h"
+
+UCLASS()
+class UA1GameplayAbility_Utility_FlashLight_Charge : public UA1GameplayAbility_Equipment
+{
+	GENERATED_BODY()
+	
+public:
+	UA1GameplayAbility_Utility_FlashLight_Charge(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+protected:
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	
+	UFUNCTION()
+	void OnMontageFinished();
+
+protected:
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<UAnimMontage> ChargeMontage;
+};
