9bdc987cbc22a0951dbd19e93d898c9a99586036
Jo-Jaehyeon <jerryjo0906@gmail.com>
2025-05-22 12:01:29 +0900

    [Add] Notice Widget Base

---
+++ Source/A1Game/UI/HUD/A1NoticeWidget.cpp
@@ -0,0 +1,9 @@
+// Copyright (c) 2025 THIS-ACCENT. All Rights Reserved.
+
+
+#include "UI/HUD/A1NoticeWidget.h"
+
+UA1NoticeWidget::UA1NoticeWidget(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
---
+++ Source/A1Game/UI/HUD/A1NoticeWidget.h
@@ -0,0 +1,22 @@
+// Copyright (c) 2025 THIS-ACCENT. All Rights Reserved.
+
+#pragma once
+
+#include "Blueprint/UserWidget.h"
+#include "A1NoticeWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class A1GAME_API UA1NoticeWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UA1NoticeWidget(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+protected:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<class UTextBlock> TxtNotice;	
+};
