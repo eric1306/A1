8009237e6b034a13eb3c78cce708da12b02585a2
PRID <prid1306@gmail.com>
2025-06-17 19:46:26 +0900

    [Fix] Overlap이벤트로 태그 부여하기 때문에 GA 태그부여 로직 제거

--- Source/A1Game/Interaction/Abilities/A1GameplayAbility_Interact_Flash_Charge.cpp
+++ Source/A1Game/Interaction/Abilities/A1GameplayAbility_Interact_Flash_Charge.cpp
@@ -30,24 +30,11 @@ void UA1GameplayAbility_Interact_Flash_Charge::ActivateAbility(const FGameplayAb
 		EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
 		return;
 	}
-
-	// 충전 가능 태그 부여
-	if (UAbilitySystemComponent* ASC = GetAbilitySystemComponentFromActorInfo())
-	{
-		ASC->AddLooseGameplayTag(A1GameplayTags::Status_FlashLight_ChargingZone);
-	}
 }
 
 void UA1GameplayAbility_Interact_Flash_Charge::EndAbility(const FGameplayAbilitySpecHandle Handle,
 	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
 	bool bReplicateEndAbility, bool bWasCancelled)
 {
-	// 충전 가능 태그 제거
-	if (UAbilitySystemComponent* ASC = GetAbilitySystemComponentFromActorInfo())
-	{
-		ASC->RemoveLooseGameplayTag(A1GameplayTags::Status_FlashLight_ChargingZone);
-	}
-
-
 	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
 }
